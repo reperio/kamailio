db830bea597fe459ee5c90d52496ded71522dc7c
Noah Mehl <noahmehl@tinyhomeconsultingllc.com>
2020-08-21 17:16:32 -0400

    Update userblacklist-create.sql for SEQUENCE statements

--- utils/kamctl/postgres/userblacklist-create.sql
+++ utils/kamctl/postgres/userblacklist-create.sql
@@ -1,21 +1,29 @@
+CREATE SEQUENCE userblacklist_id_seq;
+
 CREATE TABLE userblacklist (
-    id SERIAL PRIMARY KEY NOT NULL,
+    id integer PRIMARY KEY NOT NULL DEFAULT nextval('userblacklist_id_seq'),
     username VARCHAR(64) DEFAULT '' NOT NULL,
     domain VARCHAR(64) DEFAULT '' NOT NULL,
     prefix VARCHAR(64) DEFAULT '' NOT NULL,
     whitelist SMALLINT DEFAULT 0 NOT NULL
 );
 
+ALTER SEQUENCE userblacklist_id_seq OWNED BY userblacklist.id;
+
 CREATE INDEX userblacklist_userblacklist_idx ON userblacklist (username, domain, prefix);
 
 INSERT INTO version (table_name, table_version) values ('userblacklist','1');
 
+CREATE SEQUENCE globalblacklist_id_seq;
+
 CREATE TABLE globalblacklist (
-    id SERIAL PRIMARY KEY NOT NULL,
+    id integer PRIMARY KEY NOT NULL DEFAULT nextval('globalblacklist_id_seq'),
     prefix VARCHAR(64) DEFAULT '' NOT NULL,
     whitelist SMALLINT DEFAULT 0 NOT NULL,
     description VARCHAR(255) DEFAULT NULL
 );
+
+ALTER SEQUENCE globalblacklist_id_seq OWNED BY globalblacklist.id;
 
 CREATE INDEX globalblacklist_globalblacklist_idx ON globalblacklist (prefix);
 
