509386c4a61f9db2207ab346827ed0490705c046
Noah Mehl <noahmehl@tinyhomeconsultingllc.com>
2020-08-21 16:52:45 -0400

    Update pdt-create.sql for SEQUENCE statements

--- utils/kamctl/postgres/pdt-create.sql
+++ utils/kamctl/postgres/pdt-create.sql
@@ -1,10 +1,14 @@
+CREATE SEQUENCE pdt_id_seq;
+
 CREATE TABLE pdt (
-    id SERIAL PRIMARY KEY NOT NULL,
+    id integer PRIMARY KEY NOT NULL DEFAULT nextval('pdt_id_seq'),
     sdomain VARCHAR(255) NOT NULL,
     prefix VARCHAR(32) NOT NULL,
     domain VARCHAR(255) DEFAULT '' NOT NULL,
     CONSTRAINT pdt_sdomain_prefix_idx UNIQUE (sdomain, prefix)
 );
+
+ALTER SEQUENCE pdt_id_seq OWNED BY pdt.id;
 
 INSERT INTO version (table_name, table_version) values ('pdt','1');
 
