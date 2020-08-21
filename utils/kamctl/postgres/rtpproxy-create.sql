3ae292fcaa19b201294de10d7dab678f9f29d682
Noah Mehl <noahmehl@tinyhomeconsultingllc.com>
2020-08-21 17:03:11 -0400

    Update rtpproxy-create.sql for SEQUENCE statements

--- utils/kamctl/postgres/rtpproxy-create.sql
+++ utils/kamctl/postgres/rtpproxy-create.sql
@@ -1,11 +1,15 @@
+CREATE SEQUENCE rtpproxy_id_seq;
+
 CREATE TABLE rtpproxy (
-    id SERIAL PRIMARY KEY NOT NULL,
+    id integer PRIMARY KEY NOT NULL DEFAULT nextval('rtpproxy_id_seq'),
     setid VARCHAR(32) DEFAULT '00' NOT NULL,
     url VARCHAR(64) DEFAULT '' NOT NULL,
     flags INTEGER DEFAULT 0 NOT NULL,
     weight INTEGER DEFAULT 1 NOT NULL,
     description VARCHAR(64) DEFAULT '' NOT NULL
 );
+
+ALTER SEQUENCE rtpproxy_id_seq OWNED BY rtpproxy.id;
 
 INSERT INTO version (table_name, table_version) values ('rtpproxy','1');
 
