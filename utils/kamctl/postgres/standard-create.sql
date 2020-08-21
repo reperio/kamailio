bfa2f88bee9c844cdeff4fd05b22115ff369caee
Noah Mehl <noahmehl@tinyhomeconsultingllc.com>
2020-08-21 17:06:55 -0400

    Update standard-create.sql for SEQUENCE statements

--- utils/kamctl/postgres/standard-create.sql
+++ utils/kamctl/postgres/standard-create.sql
@@ -1,9 +1,13 @@
+CREATE SEQUENCE version_id_seq;
+
 CREATE TABLE version (
-    id SERIAL PRIMARY KEY NOT NULL,
+    id integer PRIMARY KEY NOT NULL DEFAULT nextval('version_id_seq'),
     table_name VARCHAR(32) NOT NULL,
     table_version INTEGER DEFAULT 0 NOT NULL,
     CONSTRAINT version_table_name_idx UNIQUE (table_name)
 );
+
+ALTER SEQUENCE version_id_seq OWNED BY version.id;
 
 INSERT INTO version (table_name, table_version) values ('version','1');
 
