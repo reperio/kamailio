91a761be2346b63eabd4772bd7eb8d7fec61b833
Noah Mehl <noahmehl@tinyhomeconsultingllc.com>
2020-08-21 17:14:42 -0400

    Update uid_uri_db-create.sql for SEQUENCE statements

--- utils/kamctl/postgres/uid_uri_db-create.sql
+++ utils/kamctl/postgres/uid_uri_db-create.sql
@@ -1,5 +1,7 @@
+CREATE SEQUENCE uid_uri_id_seq;
+
 CREATE TABLE uid_uri (
-    id SERIAL PRIMARY KEY NOT NULL,
+    id integer PRIMARY KEY NOT NULL DEFAULT nextval('uid_uri_id_seq'),
     uid VARCHAR(64) NOT NULL,
     did VARCHAR(64) NOT NULL,
     username VARCHAR(64) NOT NULL,
@@ -7,13 +9,17 @@ CREATE TABLE uid_uri (
     scheme VARCHAR(8) DEFAULT 'sip' NOT NULL
 );
 
+ALTER SEQUENCE uid_uri_id_seq OWNED BY uid_uri.id;
+
 CREATE INDEX uid_uri_uri_idx1 ON uid_uri (username, did, scheme);
 CREATE INDEX uid_uri_uri_uid ON uid_uri (uid);
 
 INSERT INTO version (table_name, table_version) values ('uid_uri','3');
 
+CREATE SEQUENCE uid_uri_attrs_id_seq;
+
 CREATE TABLE uid_uri_attrs (
-    id SERIAL PRIMARY KEY NOT NULL,
+    id integer PRIMARY KEY NOT NULL DEFAULT nextval('uid_uri_attrs_id_seq'),
     username VARCHAR(64) NOT NULL,
     did VARCHAR(64) NOT NULL,
     name VARCHAR(32) NOT NULL,
@@ -23,6 +29,8 @@ CREATE TABLE uid_uri_attrs (
     scheme VARCHAR(8) DEFAULT 'sip' NOT NULL,
     CONSTRAINT uid_uri_attrs_uriattrs_idx UNIQUE (username, did, name, value, scheme)
 );
+
+ALTER SEQUENCE uid_uri_attrs_id_seq OWNED BY uid_uri_attrs.id;
 
 INSERT INTO version (table_name, table_version) values ('uid_uri_attrs','2');
 
