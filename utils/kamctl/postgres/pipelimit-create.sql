5c7c828a363ed3035f96cf730ba8c9aded92cb3f
Noah Mehl <noahmehl@tinyhomeconsultingllc.com>
2020-08-21 16:55:38 -0400

    Update pipelimit-create.sql for SEQUENCE statements

--- utils/kamctl/postgres/pipelimit-create.sql
+++ utils/kamctl/postgres/pipelimit-create.sql
@@ -1,9 +1,13 @@
+CREATE SEQUENCE pl_pipes_id_seq;
+
 CREATE TABLE pl_pipes (
-    id SERIAL PRIMARY KEY NOT NULL,
+    id integer PRIMARY KEY NOT NULL DEFAULT nextval('pl_pipes_id_seq'),,
     pipeid VARCHAR(64) DEFAULT '' NOT NULL,
     algorithm VARCHAR(32) DEFAULT '' NOT NULL,
     plimit INTEGER DEFAULT 0 NOT NULL
 );
+
+ALTER SEQUENCE pl_pipes_id_seq OWNED BY pl_pipes.id;
 
 INSERT INTO version (table_name, table_version) values ('pl_pipes','1');
 
