24cc2fdfd2df2b23e5b8a17eb82c7467624b92b9
Noah Mehl <noahmehl@tinyhomeconsultingllc.com>
2020-08-21 16:59:14 -0400

    Update purple-create.sql for SEQUENCE statements

--- utils/kamctl/postgres/purple-create.sql
+++ utils/kamctl/postgres/purple-create.sql
@@ -1,10 +1,14 @@
+CREATE SEQUENCE purplemap_id_seq;
+
 CREATE TABLE purplemap (
-    id SERIAL PRIMARY KEY NOT NULL,
+    id integer PRIMARY KEY NOT NULL DEFAULT nextval('purplemap_id_seq'),
     sip_user VARCHAR(255) NOT NULL,
     ext_user VARCHAR(255) NOT NULL,
     ext_prot VARCHAR(16) NOT NULL,
     ext_pass VARCHAR(64)
 );
+
+ALTER SEQUENCE purplemap_id_seq OWNED BY purplemap.id;
 
 INSERT INTO version (table_name, table_version) values ('purplemap','1');
 
