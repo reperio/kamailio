eb7d1112afe062779c6f966985ec6114b6ac60e4
Noah Mehl <noahmehl@tinyhomeconsultingllc.com>
2020-08-21 15:50:50 -0400

    Update mohqueue-create.sql for SEQUENCE statements

--- utils/kamctl/postgres/mohqueue-create.sql
+++ utils/kamctl/postgres/mohqueue-create.sql
@@ -1,5 +1,7 @@
+CREATE SEQUENCE mohqcalls_id_seq;
+
 CREATE TABLE mohqcalls (
-    id SERIAL PRIMARY KEY NOT NULL,
+    id integer PRIMARY KEY NOT NULL DEFAULT nextval('mohqcalls_id_seq'),
     mohq_id INTEGER NOT NULL,
     call_id VARCHAR(100) NOT NULL,
     call_status INTEGER NOT NULL,
@@ -9,10 +11,14 @@ CREATE TABLE mohqcalls (
     CONSTRAINT mohqcalls_mohqcalls_idx UNIQUE (call_id)
 );
 
+ALTER SEQUENCE mohqcalls_id_seq OWNED BY mohqcalls.id;
+
 INSERT INTO version (table_name, table_version) values ('mohqcalls','1');
 
+CREATE SEQUENCE mohqueues_id_seq;
+
 CREATE TABLE mohqueues (
-    id SERIAL PRIMARY KEY NOT NULL,
+    id integer PRIMARY KEY NOT NULL DEFAULT nextval('mohqueues_id_seq'),
     name VARCHAR(25) NOT NULL,
     uri VARCHAR(100) NOT NULL,
     mohdir VARCHAR(100),
@@ -21,6 +27,8 @@ CREATE TABLE mohqueues (
     CONSTRAINT mohqueues_mohqueue_uri_idx UNIQUE (uri),
     CONSTRAINT mohqueues_mohqueue_name_idx UNIQUE (name)
 );
+
+ALTER SEQUENCE mohqueues_id_seq OWNED BY mohqueues.id;
 
 INSERT INTO version (table_name, table_version) values ('mohqueues','1');
 
