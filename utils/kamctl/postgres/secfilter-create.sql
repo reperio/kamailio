3094174d4bd5bf3bca9cd04bad3bb05f261c94b1
Noah Mehl <noahmehl@tinyhomeconsultingllc.com>
2020-08-21 17:04:39 -0400

    Update secfilter-create.sql for SEQUENCE statements

--- utils/kamctl/postgres/secfilter-create.sql
+++ utils/kamctl/postgres/secfilter-create.sql
@@ -1,9 +1,13 @@
+CREATE SEQUENCE secfilter_id_seq;
+
 CREATE TABLE secfilter (
-    id SERIAL PRIMARY KEY NOT NULL,
+    id integer PRIMARY KEY NOT NULL DEFAULT nextval('secfilter_id_seq'),
     action SMALLINT DEFAULT 0 NOT NULL,
     type SMALLINT DEFAULT 0 NOT NULL,
     data VARCHAR(64) DEFAULT '' NOT NULL
 );
+
+ALTER SEQUENCE secfilter_id_seq OWNED BY secfilter.id;
 
 CREATE INDEX secfilter_secfilter_idx ON secfilter (action, type, data);
 
