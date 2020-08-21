59eb0c2e83b01524db46c6117f1746fafe2a8835
Noah Mehl <noahmehl@tinyhomeconsultingllc.com>
2020-08-21 17:13:34 -0400

    Update uid_gflags-create.sql for SEQUENCE statements

--- utils/kamctl/postgres/uid_gflags-create.sql
+++ utils/kamctl/postgres/uid_gflags-create.sql
@@ -1,11 +1,15 @@
+CREATE SEQUENCE uid_global_attrs_id_seq;
+
 CREATE TABLE uid_global_attrs (
-    id SERIAL PRIMARY KEY NOT NULL,
+    id integer PRIMARY KEY NOT NULL DEFAULT nextval('uid_global_attrs_id_seq'),
     name VARCHAR(32) NOT NULL,
     type INTEGER DEFAULT 0 NOT NULL,
     value VARCHAR(128),
     flags INTEGER DEFAULT 0 NOT NULL,
     CONSTRAINT uid_global_attrs_global_attrs_idx UNIQUE (name, value)
 );
+
+ALTER SEQUENCE uid_global_attrs_id_seq OWNED BY uid_global_attrs.id;
 
 INSERT INTO version (table_name, table_version) values ('uid_global_attrs','1');
 
