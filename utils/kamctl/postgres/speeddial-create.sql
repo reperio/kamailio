b888dd18f499a6933394bfa6d7c67cac9827e59a
Noah Mehl <noahmehl@tinyhomeconsultingllc.com>
2020-08-21 17:06:08 -0400

    Update speeddial-create.sql for SEQUENCE statements

--- utils/kamctl/postgres/speeddial-create.sql
+++ utils/kamctl/postgres/speeddial-create.sql
@@ -1,5 +1,7 @@
+CREATE SEQUENCE speed_dial_id_seq;
+
 CREATE TABLE speed_dial (
-    id SERIAL PRIMARY KEY NOT NULL,
+    id integer PRIMARY KEY NOT NULL DEFAULT nextval('speed_dial_id_seq'),
     username VARCHAR(64) DEFAULT '' NOT NULL,
     domain VARCHAR(64) DEFAULT '' NOT NULL,
     sd_username VARCHAR(64) DEFAULT '' NOT NULL,
@@ -10,6 +12,8 @@ CREATE TABLE speed_dial (
     description VARCHAR(64) DEFAULT '' NOT NULL,
     CONSTRAINT speed_dial_speed_dial_idx UNIQUE (username, domain, sd_domain, sd_username)
 );
+
+ALTER SEQUENCE speed_dial_id_seq OWNED BY speed_dial.id;
 
 INSERT INTO version (table_name, table_version) values ('speed_dial','2');
 
