d0287959962730ee40e7d9fe308a63f494560a50
Noah Mehl <noahmehl@tinyhomeconsultingllc.com>
2020-08-21 17:10:48 -0400

    Update uid_avp_db-create.sql for SEQUENCE statements

--- utils/kamctl/postgres/uid_avp_db-create.sql
+++ utils/kamctl/postgres/uid_avp_db-create.sql
@@ -1,5 +1,7 @@
+CREATE SEQUENCE uid_user_attrs_id_seq;
+
 CREATE TABLE uid_user_attrs (
-    id SERIAL PRIMARY KEY NOT NULL,
+    id integer PRIMARY KEY NOT NULL DEFAULT nextval('uid_user_attrs_id_seq'),
     uid VARCHAR(64) NOT NULL,
     name VARCHAR(32) NOT NULL,
     value VARCHAR(128),
@@ -7,6 +9,8 @@ CREATE TABLE uid_user_attrs (
     flags INTEGER DEFAULT 0 NOT NULL,
     CONSTRAINT uid_user_attrs_userattrs_idx UNIQUE (uid, name, value)
 );
+
+ALTER SEQUENCE uid_user_attrs_id_seq OWNED BY uid_user_attrs.id;
 
 INSERT INTO version (table_name, table_version) values ('uid_user_attrs','3');
 
