155112cd51b5d25baa248671a93c205ce9c68a26
Noah Mehl <noahmehl@tinyhomeconsultingllc.com>
2020-08-21 16:54:16 -0400

    Update permissions-create.sql for SEQUENCE statements

--- utils/kamctl/postgres/permissions-create.sql
+++ utils/kamctl/postgres/permissions-create.sql
@@ -1,5 +1,7 @@
+CREATE SEQUENCE trusted_id_seq;
+
 CREATE TABLE trusted (
-    id SERIAL PRIMARY KEY NOT NULL,
+    id integer PRIMARY KEY NOT NULL DEFAULT nextval('trusted_id_seq'),
     src_ip VARCHAR(50) NOT NULL,
     proto VARCHAR(4) NOT NULL,
     from_pattern VARCHAR(64) DEFAULT NULL,
@@ -8,18 +10,24 @@ CREATE TABLE trusted (
     priority INTEGER DEFAULT 0 NOT NULL
 );
 
+ALTER SEQUENCE trusted_id_seq OWNED BY trusted.id;
+
 CREATE INDEX trusted_peer_idx ON trusted (src_ip);
 
 INSERT INTO version (table_name, table_version) values ('trusted','6');
 
+CREATE SEQUENCE address_id_seq;
+
 CREATE TABLE address (
-    id SERIAL PRIMARY KEY NOT NULL,
+    id integer PRIMARY KEY NOT NULL DEFAULT nextval('address_id_seq'),
     grp INTEGER DEFAULT 1 NOT NULL,
     ip_addr VARCHAR(50) NOT NULL,
     mask INTEGER DEFAULT 32 NOT NULL,
     port SMALLINT DEFAULT 0 NOT NULL,
     tag VARCHAR(64)
 );
+
+ALTER SEQUENCE address_id_seq OWNED BY address.id;
 
 INSERT INTO version (table_name, table_version) values ('address','6');
 
