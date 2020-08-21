880fd7e10d1329e6e276a822e1ef9654b67e4307
Noah Mehl <noahmehl@tinyhomeconsultingllc.com>
2020-08-21 15:49:37 -0400

    Update matrix-create.sql for SEQUENCE statements

--- utils/kamctl/postgres/matrix-create.sql
+++ utils/kamctl/postgres/matrix-create.sql
@@ -1,9 +1,13 @@
+CREATE SEQUENCE matrix_id_seq;
+
 CREATE TABLE matrix (
-    id SERIAL PRIMARY KEY NOT NULL,
+    id integer PRIMARY KEY NOT NULL DEFAULT nextval('matrix_id_seq'),
     first INTEGER NOT NULL,
     second SMALLINT NOT NULL,
     res INTEGER NOT NULL
 );
+
+ALTER SEQUENCE matrix_id_seq OWNED BY matrix.id;
 
 CREATE INDEX matrix_matrix_idx ON matrix (first, second);
 
