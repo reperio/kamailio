ab7070bd70a2439e05987f1e2bb032309439f355
Noah Mehl <noahmehl@tinyhomeconsultingllc.com>
2020-08-21 15:42:34 -0400

    Update group-create.sql for SEQUENCE statements

--- utils/kamctl/postgres/group-create.sql
+++ utils/kamctl/postgres/group-create.sql
@@ -1,5 +1,7 @@
+CREATE SEQUENCE grp_id_seq;
+
 CREATE TABLE grp (
-    id SERIAL PRIMARY KEY NOT NULL,
+    id integer PRIMARY KEY NOT NULL DEFAULT nextval('grp_id_seq'),
     username VARCHAR(64) DEFAULT '' NOT NULL,
     domain VARCHAR(64) DEFAULT '' NOT NULL,
     grp VARCHAR(64) DEFAULT '' NOT NULL,
@@ -7,13 +9,19 @@ CREATE TABLE grp (
     CONSTRAINT grp_account_group_idx UNIQUE (username, domain, grp)
 );
 
+ALTER SEQUENCE grp_id_seq OWNED BY grp.id;
+
 INSERT INTO version (table_name, table_version) values ('grp','2');
 
+CREATE SEQUENCE re_grp_id_seq;
+
 CREATE TABLE re_grp (
-    id SERIAL PRIMARY KEY NOT NULL,
+    id integer PRIMARY KEY NOT NULL DEFAULT nextval('re_grp_id_seq'),
     reg_exp VARCHAR(128) DEFAULT '' NOT NULL,
     group_id INTEGER DEFAULT 0 NOT NULL
 );
+
+ALTER SEQUENCE re_grp_id_seq OWNED BY re_grp.id;
 
 CREATE INDEX re_grp_group_idx ON re_grp (group_id);
 
