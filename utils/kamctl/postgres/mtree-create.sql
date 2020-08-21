1561c8973916c25f72dac4c32e095daa7c33959b
Noah Mehl <noahmehl@tinyhomeconsultingllc.com>
2020-08-21 15:57:35 -0400

    Update mtree-create.sql for SEQUENCE statements

--- utils/kamctl/postgres/mtree-create.sql
+++ utils/kamctl/postgres/mtree-create.sql
@@ -1,19 +1,27 @@
+CREATE SEQUENCE mtree_id_seq;
+
 CREATE TABLE mtree (
-    id SERIAL PRIMARY KEY NOT NULL,
+    id integer PRIMARY KEY NOT NULL DEFAULT nextval('mtree_id_seq'),
     tprefix VARCHAR(32) DEFAULT '' NOT NULL,
     tvalue VARCHAR(128) DEFAULT '' NOT NULL,
     CONSTRAINT mtree_tprefix_idx UNIQUE (tprefix)
 );
 
+ALTER SEQUENCE mtree_id_seq OWNED BY mtree.id;
+
 INSERT INTO version (table_name, table_version) values ('mtree','1');
 
+CREATE SEQUENCE mtrees_id_seq;
+
 CREATE TABLE mtrees (
-    id SERIAL PRIMARY KEY NOT NULL,
+    id integer PRIMARY KEY NOT NULL DEFAULT nextval('mtrees_id_seq'),
     tname VARCHAR(128) DEFAULT '' NOT NULL,
     tprefix VARCHAR(32) DEFAULT '' NOT NULL,
     tvalue VARCHAR(128) DEFAULT '' NOT NULL,
     CONSTRAINT mtrees_tname_tprefix_tvalue_idx UNIQUE (tname, tprefix, tvalue)
 );
+
+ALTER SEQUENCE mtrees_id_seq OWNED BY mtrees.id;
 
 INSERT INTO version (table_name, table_version) values ('mtrees','2');
 
