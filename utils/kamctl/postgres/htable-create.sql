c405a511e4dda37ae6f56b93fb9f82986245ac15
Noah Mehl <noahmehl@tinyhomeconsultingllc.com>
2020-08-21 15:45:29 -0400

    Update htable-create.sql for SEQUENCE statements

--- utils/kamctl/postgres/htable-create.sql
+++ utils/kamctl/postgres/htable-create.sql
@@ -1,11 +1,15 @@
+CREATE SEQUENCE htable_id_seq;
+
 CREATE TABLE htable (
-    id SERIAL PRIMARY KEY NOT NULL,
+    id integer PRIMARY KEY NOT NULL DEFAULT nextval('htable_id_seq'),
     key_name VARCHAR(64) DEFAULT '' NOT NULL,
     key_type INTEGER DEFAULT 0 NOT NULL,
     value_type INTEGER DEFAULT 0 NOT NULL,
     key_value VARCHAR(128) DEFAULT '' NOT NULL,
     expires INTEGER DEFAULT 0 NOT NULL
 );
+
+ALTER SEQUENCE htable_id_seq OWNED BY htable.id;
 
 INSERT INTO version (table_name, table_version) values ('htable','2');
 
