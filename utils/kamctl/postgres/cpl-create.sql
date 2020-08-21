0f88ef80d3b1a9d74dd5dbbb4fbd18154f7635e8
Noah Mehl <noahmehl@tinyhomeconsultingllc.com>
2020-08-21 15:31:38 -0400

    Update cpl-create.sql for SEQUENCE statements

--- utils/kamctl/postgres/cpl-create.sql
+++ utils/kamctl/postgres/cpl-create.sql
@@ -1,11 +1,15 @@
+CREATE SEQUENCE cpl_id_seq;
+
 CREATE TABLE cpl (
-    id SERIAL PRIMARY KEY NOT NULL,
+    id integer PRIMARY KEY NOT NULL DEFAULT nextval('cpl_id_seq'),
     username VARCHAR(64) NOT NULL,
     domain VARCHAR(64) DEFAULT '' NOT NULL,
     cpl_xml TEXT,
     cpl_bin TEXT,
     CONSTRAINT cpl_account_idx UNIQUE (username, domain)
 );
+
+ALTER SEQUENCE cpl_id_seq OWNED BY cpl.id;
 
 INSERT INTO version (table_name, table_version) values ('cpl','1');
 
