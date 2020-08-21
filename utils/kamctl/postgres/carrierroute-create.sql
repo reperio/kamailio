7baad6e9227a4064daac1b85aaee2f8ad9193610
Noah Mehl <noahmehl@tinyhomeconsultingllc.com>
2020-08-21 15:30:40 -0400

    Update carrierroute-create.sql for SEQUENCE statements

--- utils/kamctl/postgres/carrierroute-create.sql
+++ utils/kamctl/postgres/carrierroute-create.sql
@@ -1,5 +1,7 @@
+CREATE SEQUENCE carrierroute_id_seq;
+
 CREATE TABLE carrierroute (
-    id SERIAL PRIMARY KEY NOT NULL,
+    id integer PRIMARY KEY NOT NULL DEFAULT nextval('carrierroute_id_seq'),
     carrier INTEGER DEFAULT 0 NOT NULL,
     domain INTEGER DEFAULT 0 NOT NULL,
     scan_prefix VARCHAR(64) DEFAULT '' NOT NULL,
@@ -13,10 +15,14 @@ CREATE TABLE carrierroute (
     description VARCHAR(255) DEFAULT NULL
 );
 
+ALTER SEQUENCE carrierroute_id_seq OWNED BY carrierroute.id;
+
 INSERT INTO version (table_name, table_version) values ('carrierroute','3');
 
+CREATE SEQUENCE carrierfailureroute_id_seq;
+
 CREATE TABLE carrierfailureroute (
-    id SERIAL PRIMARY KEY NOT NULL,
+    id integer PRIMARY KEY NOT NULL DEFAULT nextval('carrierfailureroute_id_seq'),
     carrier INTEGER DEFAULT 0 NOT NULL,
     domain INTEGER DEFAULT 0 NOT NULL,
     scan_prefix VARCHAR(64) DEFAULT '' NOT NULL,
@@ -28,6 +34,8 @@ CREATE TABLE carrierfailureroute (
     description VARCHAR(255) DEFAULT NULL
 );
 
+ALTER SEQUENCE carrierfailureroute_id_seq OWNED BY carrierfailureroute.id;
+
 INSERT INTO version (table_name, table_version) values ('carrierfailureroute','2');
 
 CREATE TABLE carrier_name (
@@ -37,10 +45,14 @@ CREATE TABLE carrier_name (
 
 INSERT INTO version (table_name, table_version) values ('carrier_name','1');
 
+CREATE SEQUENCE domain_name_id_seq;
+
 CREATE TABLE domain_name (
-    id SERIAL PRIMARY KEY NOT NULL,
+    id integer PRIMARY KEY NOT NULL DEFAULT nextval('domain_name_id_seq'),
     domain VARCHAR(64) DEFAULT NULL
 );
+
+ALTER SEQUENCE domain_name_id_seq OWNED BY domain_name.id;
 
 INSERT INTO version (table_name, table_version) values ('domain_name','1');
 
