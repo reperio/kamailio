15ef1f2c4fb0d0d911e5940e5b5e660ad876c1ac
Noah Mehl <noahmehl@tinyhomeconsultingllc.com>
2020-08-21 17:02:28 -0400

    Update rtpengine-create.sql for SEQUENCE statements

--- utils/kamctl/postgres/rtpengine-create.sql
+++ utils/kamctl/postgres/rtpengine-create.sql
@@ -1,5 +1,7 @@
+CREATE SEQUENCE rtpengine_id_seq;
+
 CREATE TABLE rtpengine (
-    id SERIAL PRIMARY KEY NOT NULL,
+    id integer PRIMARY KEY NOT NULL DEFAULT nextval('rtpengine_id_seq'),
     setid INTEGER DEFAULT 0 NOT NULL,
     url VARCHAR(64) NOT NULL,
     weight INTEGER DEFAULT 1 NOT NULL,
@@ -7,6 +9,8 @@ CREATE TABLE rtpengine (
     stamp TIMESTAMP WITHOUT TIME ZONE DEFAULT '1900-01-01 00:00:01' NOT NULL,
     CONSTRAINT rtpengine_rtpengine_nodes UNIQUE (setid, url)
 );
+
+ALTER SEQUENCE rtpengine_id_seq OWNED BY rtpengine.id;
 
 INSERT INTO version (table_name, table_version) values ('rtpengine','1');
 
