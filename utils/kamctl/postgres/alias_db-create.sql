8a02a0b3af45ad858ac9f916b6edc7c9e3642435
Noah Mehl <noahmehl@tinyhomeconsultingllc.com>
2020-08-21 15:21:31 -0400

    Update alias_db-create.sql for SEQUENCE statements

--- utils/kamctl/postgres/alias_db-create.sql
+++ utils/kamctl/postgres/alias_db-create.sql
@@ -1,10 +1,14 @@
+CREATE SEQUENCE dbaliases_id_seq;
+
 CREATE TABLE dbaliases (
-    id SERIAL PRIMARY KEY NOT NULL,
+    id integer PRIMARY KEY NOT NULL DEFAULT nextval('dbaliases_id_seq'),
     alias_username VARCHAR(64) DEFAULT '' NOT NULL,
     alias_domain VARCHAR(64) DEFAULT '' NOT NULL,
     username VARCHAR(64) DEFAULT '' NOT NULL,
     domain VARCHAR(64) DEFAULT '' NOT NULL
 );
+
+ALTER SEQUENCE dbaliases_id_seq OWNED BY dbaliases.id;
 
 CREATE INDEX dbaliases_alias_user_idx ON dbaliases (alias_username);
 CREATE INDEX dbaliases_alias_idx ON dbaliases (alias_username, alias_domain);
